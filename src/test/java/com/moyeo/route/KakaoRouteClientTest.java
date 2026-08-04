920dc7d5e6fefeae8703155fa66cd9b9e9475b9d
ohujj <114215140+ohujj@users.noreply.github.com>
2026-08-04 19:58:30 +0900

    test: 경로 도보 폴백 분기 검증

--- src/test/java/com/moyeo/route/KakaoRouteClientTest.java
+++ src/test/java/com/moyeo/route/KakaoRouteClientTest.java
@@ -15,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.queryParam;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 class KakaoRouteClientTest {
@@ -62,6 +63,23 @@ class KakaoRouteClientTest {
     }
 
     @Test
+    void publicTransitRouteUsesTransitDurationWithoutWalkingFallback() {
+        expectRouteRequest("/v2/routing/publictraffic")
+                .andRespond(withSuccess("""
+                        {"status":"OK","routes":[{"properties":{"totalTime":630}}]}
+                        """, MediaType.APPLICATION_JSON));
+
+        long duration = client.findShortestTravelTimeSeconds(
+                TransportationMode.PUBLIC_TRANSIT,
+                new BigDecimal("37.5202231"), new BigDecimal("126.8528896"),
+                new BigDecimal("37.5218464"), new BigDecimal("126.8526352")
+        );
+
+        assertThat(duration).isEqualTo(630);
+        server.verify();
+    }
+
+    @Test
     void walkingNoResultsRemainsUnavailable() {
         expectRouteRequest("/v2/routing/publictraffic")
                 .andRespond(withSuccess("""
@@ -77,6 +95,38 @@ class KakaoRouteClientTest {
                 new BigDecimal("37.5202231"), new BigDecimal("126.8528896"),
                 new BigDecimal("37.5218464"), new BigDecimal("126.8526352")
         )).isInstanceOf(KakaoRouteUnavailableException.class);
+        server.verify();
+    }
+
+    @Test
+    void publicTransitHttpFailureDoesNotAttemptWalkingFallback() {
+        expectRouteRequest("/v2/routing/publictraffic").andRespond(withServerError());
+
+        assertThatThrownBy(() -> client.findShortestTravelTimeSeconds(
+                TransportationMode.PUBLIC_TRANSIT,
+                new BigDecimal("37.5202231"), new BigDecimal("126.8528896"),
+                new BigDecimal("37.5218464"), new BigDecimal("126.8526352")
+        )).isInstanceOf(KakaoRouteUnavailableException.class);
+        server.verify();
+    }
+
+    @Test
+    void carRouteContinuesToUseNaviDirections() {
+        server.expect(request -> assertThat(request.getURI().getPath()).isEqualTo("/v1/directions"))
+                .andExpect(queryParam("origin", "126.8528896,37.5202231"))
+                .andExpect(queryParam("destination", "126.8526352,37.5218464"))
+                .andExpect(header("Authorization", "KakaoAK test-rest-api-key"))
+                .andRespond(withSuccess("""
+                        {"routes":[{"summary":{"duration":480}}]}
+                        """, MediaType.APPLICATION_JSON));
+
+        long duration = client.findShortestTravelTimeSeconds(
+                TransportationMode.CAR,
+                new BigDecimal("37.5202231"), new BigDecimal("126.8528896"),
+                new BigDecimal("37.5218464"), new BigDecimal("126.8526352")
+        );
+
+        assertThat(duration).isEqualTo(480);
         server.verify();
     }
 
